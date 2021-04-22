20656b75a8171d371c4f61aa9e9a8c30b499c758
HolyPi <joanelly2018@gmail.com>
2021-04-21 21:21:38 -0400

    Implemented S3 file upload

--- models/pet.js
+++ models/pet.js
@@ -2,21 +2,23 @@
 
 const mongoose = require('mongoose'),
         Schema = mongoose.Schema;
+
+// initialize mongoose-paginate & set default options 
 const mongoosePaginate = require('mongoose-paginate');
 
 mongoosePaginate.paginate.options = {
   limit: 3 // how many records on each page
 };
+
 const PetSchema = new Schema({
   name: { type: String, required: true }
   , birthday: {type: String, required: true }
   , species: { type: String, required: true }
-  , picUrl: { type: String, required: true }
-  , picUrlSq: { type: String, required: true }
+  , picUrl: { type: String }
+  , picUrlSq: { type: String }
   , avatarUrl: { type: String, required: true }
   , favoriteFood: { type: String, required: true }
   , description: { type: String, minlength: 140, required: true }
-  , price: {type: Number, required: true }
 }, {
   timestamps: true
 });
--- routes/pets.js
+++ routes/pets.js
@@ -1,8 +1,34 @@
 // MODELS
 const Pet = require('../models/pet');
-
 const mailer = require('../utils/mailer');
 
+// UPLOADING TO AWS S3
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  },{
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
 
 // PET ROUTES
 module.exports = (app) => {
@@ -14,19 +40,31 @@ module.exports = (app) => {
     res.render('pets-new');
   });
 
-  // CREATE PET
-  app.post('/pets', (req, res) => {
-    var pet = new Pet(req.body);
+// CREATE PET
+app.post('/pets', upload.single('avatar'), (req, res, next) => {
+  var pet = new Pet(req.body);
+  pet.save(function (err) {
+    if (req.file) {
+      // Upload the images
+      client.upload(req.file.path, {}, function (err, versions, meta) {
+        if (err) { return res.status(400).send({ err: err }) };
 
-    pet.save()
-      .then((pet) => {
+        // Pop off the -square and -standard and just use the one URL to grab the image
+        versions.forEach(function (image) {
+          var urlArray = image.url.split('-');
+          urlArray.pop();
+          var url = urlArray.join('-');
+          pet.avatarUrl = url;
+          pet.save();
+        });
+
         res.send({ pet: pet });
-      })
-      .catch((err) => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
-      }) ;
-  });
+      });
+    } else {
+      res.send({ pet: pet });
+    }
+  })
+})
 
   // SHOW PET
   app.get('/pets/:id', (req, res) => {
@@ -35,17 +73,58 @@ module.exports = (app) => {
     });
   });
 
-// SEARCH PET
-app.get('/search', (req, res) => {
-  term = new RegExp(req.query.term, 'i')
+  // SEARCH PET
+  app.get('/search', (req, res) => {
+    const term = new RegExp(req.query.term, 'i')
 
-  Pet.find({$or:[
-    {'name': term},
-    {'species': term}
-  ]}).exec((err, pets) => {
-    res.render('pets-index', { pets: pets });
-  })
-});
+    const page = req.query.page || 1 
+    Pet.paginate(
+      {
+        // take existing $or query and put it as that query parameter for paginate
+        $or: [
+          {'name': term}, 
+          {'spaces': term}
+        ]
+      }, 
+      {page: page}).then((results) => {
+
+        // pass original term (not RegExp) to populate correct URL w/ string
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, 
+                                    currentPage: page, term: req.query.term });
+      });
+
+    // Pet.find({$or:[
+    //   {'name': term},
+    //   {'species': term}
+    // ]}).exec((err, pets) => {
+    //   res.render('pets-index', { pets: pets });
+    // })
+  });
+
+  // EDIT PET
+  app.get('/pets/:id/edit', (req, res) => {
+    Pet.findById(req.params.id).exec((err, pet) => {
+      res.render('pets-edit', { pet: pet });
+    });
+  });
+
+  // UPDATE PET
+  app.put('/pets/:id', (req, res) => {
+    Pet.findByIdAndUpdate(req.params.id, req.body)
+      .then((pet) => {
+        res.redirect(`/pets/${pet._id}`)
+      })
+      .catch((err) => {
+        // Handle Errors
+      });
+  });
+
+  // DELETE PET
+  app.delete('/pets/:id', (req, res) => {
+    Pet.findByIdAndRemove(req.params.id).exec((err, pet) => {
+      return res.redirect('/')
+    });
+  });
 
 // PURCHASE
 app.post('/pets/:id/purchase', (req, res) => {
@@ -87,33 +166,5 @@ app.post('/pets/:id/purchase', (req, res) => {
     });
   })
 });
-  // EDIT PET
-  app.get('/pets/:id/edit', (req, res) => {
-    Pet.findById(req.params.id).exec((err, pet) => {
-      res.render('pets-edit', { pet: pet });
-    });
-  });
-
-  // UPDATE PET
-  app.put('/pets/:id', (req, res) => {
-    Pet.findByIdAndUpdate(req.params.id, req.body)
-      .then((pet) => {
-        res.redirect(`/pets/${pet._id}`)
-      })
-      .catch((err) => {
-        // Handle Errors
-      });
-  });
-
-  // DELETE PET
-  app.delete('/pets/:id', (req, res) => {
-    Pet.findByIdAndRemove(req.params.id).exec((err, pet) => {
-      return res.redirect('/')
-    });
-  });
-
-
 
 }
-
-
